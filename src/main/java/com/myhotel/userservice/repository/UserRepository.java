4335bb6d7f691a95896d74700d483f530c7fe01c
Debalina <dianadebalina18@gmail.com>
2022-09-15 10:38:15 +0530

    add validation, implements swagger ui, modify some functionality.

--- src/main/java/com/myhotel/userservice/repository/UserRepository.java
+++ src/main/java/com/myhotel/userservice/repository/UserRepository.java
@@ -2,12 +2,11 @@ package com.myhotel.userservice.repository;
 
 import com.myhotel.userservice.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
-    boolean existsUserEntityByUserPhoneNumber(@Param("n") String number);
+    boolean existsUserEntityByUserPhoneNumber(String number);
 
     UserEntity findByUserPhoneNumber(String userPhoneNumber);
 }
